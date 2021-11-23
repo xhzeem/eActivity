22c893f7a8a677d25b751b18d902e6c704dac98f
Majd Alali <m-syr@outlook.sa>
2021-11-23 21:07:29 +0800

    add test 2

--- src/app/components/test2/test2.component.ts
+++ src/app/components/test2/test2.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test2',
   templateUrl: './test2.component.html',
-  styleUrls: ['./test2.component.scss']
+  styleUrls: ['./test2.component.scss'],
 })
 export class Test2Component implements OnInit {
+  doggie = '../../../assets/Imges/dog.jfif';
+  logo = '../../../assets/Imges/logo.svg';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
